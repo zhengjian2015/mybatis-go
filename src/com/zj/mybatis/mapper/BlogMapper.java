58c420ac349f29cf7be5143c42a8add82a07af75
zj21zhengj <570470242@qq.com>
2018-05-20 19:32:47 +0800

    多参数传递（分页查询）

--- src/com/zj/mybatis/mapper/BlogMapper.java
+++ src/com/zj/mybatis/mapper/BlogMapper.java
@@ -1,6 +1,9 @@
 package com.zj.mybatis.mapper;
 
 import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Param;
 
 import com.zj.mybatis.pojo.Blog;
 
@@ -10,4 +13,14 @@ public interface BlogMapper {
 	List<Blog> selectBlogByTitle(String title);
 	
 	List<Blog> selectBlogByTitle2(String title);
+	
+	List<Blog> selectBlogOrderBySort(String column);
+	
+	List<Blog> selectBlogByPage(int offset,int page);
+	
+	List<Blog> selectBlogByPage2(
+			@Param(value="offeset") int offeset,
+			@Param(value="pagesize") int pagesize);
+	
+	List<Blog> selectBlogByPage3(Map<String,Object> map);
 }
--- test/com/zj/mybatis/mapper/BlogMapperTest.java
+++ test/com/zj/mybatis/mapper/BlogMapperTest.java
@@ -1,6 +1,8 @@
 package com.zj.mybatis.mapper;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
@@ -59,4 +61,59 @@ public class BlogMapperTest{
 		
 		System.out.println(blogList.toString());
 	}
-}
+	
+	@Test
+	public void testselectBlogOrderBySort() {
+		SqlSession session = MyBatisUtil.getSqlsession();
+		
+		BlogMapper blogMapper = session.getMapper(BlogMapper.class);
+		
+		List<Blog> blogList = blogMapper.selectBlogOrderBySort("state");
+		
+		session.close();
+		
+		System.out.println(blogList.toString());
+	}
+	
+	@Test
+	public void testselectBlogByPage() {
+		SqlSession session = MyBatisUtil.getSqlsession();
+		
+		BlogMapper blogMapper = session.getMapper(BlogMapper.class);
+		
+		List<Blog> blogList = blogMapper.selectBlogByPage(2, 2);
+		
+		session.close();
+		
+		System.out.println(blogList.toString());
+	}
+	
+	@Test
+	public void testselectBlogByPage2() {
+		SqlSession session = MyBatisUtil.getSqlsession();
+		
+		BlogMapper blogMapper = session.getMapper(BlogMapper.class);
+		
+		List<Blog> blogList = blogMapper.selectBlogByPage2(1, 3);
+		
+		session.close();
+		
+		System.out.println(blogList.toString());
+	}
+	
+	@Test
+	public void testselectBlogByPage3() {
+		SqlSession session = MyBatisUtil.getSqlsession();
+		
+		BlogMapper blogMapper = session.getMapper(BlogMapper.class);
+		
+		Map<String,Object> map = new HashMap<String,Object>();
+		map.put("offeset", 2);
+		map.put("pagesize", 3);
+		List<Blog> blogList = blogMapper.selectBlogByPage3(map);
+		
+		session.close();
+		
+		System.out.println(blogList.toString());
+	}
+} 
